bd9cfa995a11852302fe64e604e97101f13ae100
looppin <berksoroglu@gmail.com>
2022-01-15 17:29:25 +0300

    Create shipping method and add getLastFourDay() function. Thats mean; this function takes next four day

---
+++ catalog/language/en-gb/extension/shipping/date.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_title']       = 'Delivery Date';
+$_['text_description'] = 'Select delivery date';
---
+++ catalog/model/extension/shipping/date.php
@@ -0,0 +1,52 @@
+<?php
+class ModelExtensionShippingDate extends Model {
+	function getQuote($address) {
+		$this->load->language('extension/shipping/date');
+
+		$method_data = array();
+
+        $quote_data = array();
+
+        $lastDays = $this->getLastFourDay();
+
+        foreach( $lastDays as $day )
+        {
+            $quote_data['date_'.$day["day"]] = array(
+                'code'         => 'date.'.$day["date"],
+                'title'        => $this->language->get('text_description') . ' ' . $day["day"],
+                'cost'         => 5.00,
+                'tax_class_id' => 0,
+                'text'         => $this->currency->format(5.00, $this->session->data['currency'])
+            );
+        }
+
+        $method_data = array(
+            'code'       => 'date',
+            'title'      => $this->language->get('text_title'),
+            'quote'      => $quote_data,
+            'sort_order' => $this->config->get('shipping_date_sort_order'),
+            'error'      => false
+        );
+	
+
+		return $method_data;
+	}
+
+    public function getLastFourDay()
+    {
+        $lastDays = [];
+
+        for( $i=1; $i <= 5; $i++ )
+        {
+            $lastDays[] = [
+                'day' => date("D", time() + 86400 * $i ),
+                'date' => date("Y-m-d", time() + 86400 * $i )
+            ];
+                
+        }
+
+        return $lastDays;
+
+    }
+
+}
